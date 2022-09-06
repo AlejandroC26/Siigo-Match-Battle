3abecb0eeaea7675e9b6682ce2be39e527e38ee1
AlejandroC26 <cubillosa862@gmail.com>
2022-09-06 17:34:26 -0500

    Carta

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,21 +1,73 @@
 <template>
     <div class="card">
         <div class="card-header">
-            <div class="card-id">1 A</div>
-            <div class="card-name">VUE CARD</div>
+            <div class="card-id">{{card_info.id}}</div>
+            <div class="card-name">{{card_info.name}}</div>
         </div>
         <div class="card-image">
             <img src="../assets/img/cards/1.png">
         </div>
         <div class="card-features">
-            <h4>Features</h4>
-            <div class="feature" v-for="i in 5">
+            <h4>Características</h4>
+            <div class="feature" :class="{'ft-hover':ftHover, 'active':ftSelected.feature == 'cylinder'}" 
+            @click="returnFeature({feature: 'cylinder', value: card_info.cylinder})">
                 <div class="feature-name">  Cilindraje</div>
-                <div class="feature-value"> 1290</div>
+                <div class="feature-value"> {{card_info.cylinder}}</div>
+            </div>
+            <div class="feature" :class="{'ft-hover':ftHover, 'active':ftSelected.feature == 'top_speed'}" 
+            @click="returnFeature({feature: 'top_speed', value: card_info.top_speed})">
+                <div class="feature-name">  Top Speed</div>
+                <div class="feature-value"> {{card_info.top_speed}}</div>
+            </div>
+            <div class="feature" :class="{'ft-hover':ftHover, 'active':ftSelected.feature == 'torque'}" 
+            @click="returnFeature({feature: 'torque', value: card_info.torque})">
+                <div class="feature-name">  Torque</div>
+                <div class="feature-value"> {{card_info.torque}}</div>
+            </div>
+            <div class="feature" :class="{'ft-hover':ftHover, 'active':ftSelected.feature == 'weigh'}" 
+            @click="returnFeature({feature: 'weigh', value: card_info.weigh})">
+                <div class="feature-name">  Peso</div>
+                <div class="feature-value"> {{card_info.weigh}}</div>
+            </div>
+            <div class="feature" :class="{'ft-hover':ftHover, 'active':ftSelected.feature == 'year'}" 
+            @click="returnFeature({feature: 'year', value: card_info.year})">
+                <div class="feature-name">  Año</div>
+                <div class="feature-value"> {{card_info.year}}</div>
             </div>
         </div>
     </div>
 </template>
+<script>
+    export default {
+        props: ['info', 'putCart'],
+        data(){
+            return {
+                ftHover: false,
+                ftSelected: '',
+                card_info: {
+                    id: '',
+                    name: '',
+                    cylinder: '',
+                    year: '',
+                    top_speed: '',
+                    torque: '',
+                    weight: '',
+                }
+            }
+        },
+        methods: {
+            async returnFeature(params) {
+                this.putCart(params);
+            }
+        },
+        mounted() {
+            if(this.info.ftHover) this.ftHover = this.info.ftHover;
+            if(this.info.feature) this.ftSelected = this.info.feature;
+            console.log(this.ftSelected)
+            this.card_info = this.info.data;
+        }
+    }
+</script>
 <style scoped>
     .card{
         background: #fff;
@@ -27,6 +79,8 @@
         border-radius: .1rem;
         font-size: .95rem;
         padding: .5rem 1rem 1.5rem 1rem;
+        cursor: pointer;
+        transition: all .5s;
     }
     /* CABECERA DE LA CARTA */
     .card .card-header {
@@ -52,5 +106,14 @@
     .card .feature {
         display: flex;
         justify-content: space-between;
+        margin-bottom: .3rem;
+        transition: all .2s;
+        border-radius: 3px;
+        padding: 0 .1rem;
+    }
+    .card .ft-hover:hover,
+    .card .active {
+        background: rgb(0, 2, 3);
+        color: #fff;
     }
 </style>
--- src/views/GameVue.vue
+++ src/views/GameVue.vue
@@ -1,27 +1,37 @@
 <template>
-    <div class="game">
-        <!-- JUGADORES -->
-        <div v-for="player in players" class="player-wrapper" :class="'player-wrapper-'+i">
-            <div class="player" :class="'player'+i">
-                <div class="player-image">
-                    <img src="../assets/logo.png">
+    <div>
+        <div class="game">
+            <!-- JUGADORES -->
+            <div v-for="(player, i) in players" class="player-wrapper" :class="'player-wrapper-'+(i+1)">
+                <!--CARTAS DEL JUGADOR-->
+                <div class="player-cards" 
+                v-if="(i+1) == 1 && config.userPlaying == 1 && myCards.length ">
+                    <Card class="player-card" :info="{ftHover:true,  data: myCards[0]}" :putCart="onFeatures"></Card>
+                    <div class="false-card" style="top: 3px"></div>
+                    <div class="false-card" style="top: 5px"></div>
+                    <div class="false-card" style="top: 7px"></div>
                 </div>
-                <div class="player-info">
-                    <div class="player-name">{{player.name}}</div>
-                    <div class="player-cards">
-                        <label>logo-c</label> 3
+                <div class="player" :class="'player'+(i+1)">
+                    <div class="player-image">
+                        <img src="../assets/logo.png">
+                    </div>
+                    <div class="player-info">
+                        <div class="player-name">{{player.name}}</div>
+                        <div class="player-num-cards">
+                            <label>logo-c</label> {{player.cards}}
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <!-- JUEGO CENTRAL -->
-        <div class="cards">
-            <Card class="active-card"></Card>
-            <div class="played-cards">
-                <Card style="transform: rotateZ(-20deg);"></Card>
-                <Card style="transform: rotateZ(-8deg);"></Card>
-                <Card style="transform: rotateZ(-2deg);"></Card>
-                <Card style="transform: rotateZ(-10deg);"></Card>
+            <!-- JUEGO CENTRAL -->
+            <div class="cards">
+                <Card v-if="playedCards.length" :info="{feature: playedCards[0].feature,  data: playedCards[0].data}" class="active-card"></Card>
+                <!-- <div class="played-cards">
+                    <Card style="transform: rotateZ(-20deg);"></Card>
+                    <Card style="transform: rotateZ(-8deg);"></Card>
+                    <Card style="transform: rotateZ(-2deg);"></Card>
+                    <Card style="transform: rotateZ(-10deg);"></Card>
+                </div> -->
             </div>
         </div>
     </div>
@@ -32,17 +42,53 @@
         name: 'Game',
         data: function() {
             return {
+                config: {
+                    userPlaying: 1
+                },
+                playedCards:[],
+                myCards: [
+                    {
+                        id: '1 A',
+                        name: 'Kawasaki Ninja H2R',
+                        cylinder: 998,
+                        year: '2022-01-01',
+                        torque: 25,
+                        top_speed: 400,
+                        weigh: 500,
+                    }
+                ],
                 players: [
                     {
                         'id': 1,
                         'name': 'Alejandro',
                         'cards': 1
                     },
+                    {
+                        'id': 2,
+                        'name': 'Santiago',
+                        'cards': 1
+                    },
+                    {
+                        'id': 3,
+                        'name': 'James',
+                        'cards': 1
+                    },
                 ]
             }
         },
+        methods: {
+            onFeatures(returned) {
+                this.userPlaying = 2;
+                const result = this.myCards.filter(card => card.id != this.myCards[0].id);
+                this.playedCards.push({
+                    feature: returned,
+                    data: this.myCards[0]
+                });
+                this.myCards = result;
+            }
+        },
         components: {
-            Card
+            Card,
         }
     } 
 </script>
@@ -97,5 +143,26 @@
         z-index: 10;
         transform: scale(1.1);
         box-shadow: 0 0 20px #e4e707;
+    }
+
+    /* CARTAS DEL JUGADOR */
+    .player-cards{
+        top: -20px;
+        width: 305px;
+        position: relative;
+    }
+    .player-cards .player-card {
+        position: absolute;
+        z-index: 15;
+    }
+    .player-cards .player-card:hover {
+        transform: scale(1.3);
+    }
+    .false-card {
+        position: absolute;
+        width: 222px;
+        height: 330px;
+        background: #d3d3d3;
+        box-shadow: 0 .3rem .2rem #8d8d8d;
     }
 </style>
